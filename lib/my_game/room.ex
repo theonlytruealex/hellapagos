d8086b4637d86f16ba425ca5ede91c9068e46b68
theonlytruealex <alexiova264@gmail.com>
2025-10-27 22:00:11 +0200

    started working on game room logic

--- lib/my_game/room.ex
+++ lib/my_game/room.ex
@@ -1,12 +1,30 @@
 defmodule MyGame.Room do
   use GenServer
 
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
   def init(state) do
-    {:ok, state}
+    start_state = %{
+      players: [],
+      water_deck: [],
+      scavenge_deck: [],
+      food: 0,
+      water: 0,
+      wood: 0,
+      boats: 0
+    }
+
+    {:ok, starter_state}
   end
 
   def handle_cast({:join, player_id}, state) do
-    updated_state = [player_id | state]
+    updated_state = %{
+      state
+      | players: [player_id | state.players]
+    }
+
     {:noreply, updated_state}
   end
 
