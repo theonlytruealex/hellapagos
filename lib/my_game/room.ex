4ecd988bbcbc1aa2a2eb07026d118167bdc1fa31
theonlytruealex <alexiova264@gmail.com>
2025-10-28 09:42:55 +0200

    Working on GenServer for room

--- lib/my_game/room.ex
+++ lib/my_game/room.ex
@@ -1,13 +1,23 @@
 defmodule MyGame.Room do
   use GenServer
 
-  def start_link(_opts) do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  # Client functions
+
+  def start_link({name, players}) do
+    GenServer.start_link(__MODULE__, players, name: __MODULE__)
   end
 
-  def init(state) do
-    start_state = %{
-      players: [],
+  def update_score(pid, kind, amount) do
+    GenServer.cast(pid, {:update_stat, kind, amount})
+  end
+
+  # Server functions
+
+  @impl true
+  def init(players) do
+    starter_state = %{
+      players: players,
+      alive_players: players,
       water_deck: [],
       scavenge_deck: [],
       food: 0,
@@ -19,21 +29,72 @@ defmodule MyGame.Room do
     {:ok, starter_state}
   end
 
-  def handle_cast({:join, player_id}, state) do
-    updated_state = %{
-      state
-      | players: [player_id | state.players]
-    }
+  @impl true
+  def handle_cast({:update_stat, stat, amount}, state) do
+    updated_state =
+      Map.update!(state, stat, fn existing_value -> existing_value + amount end)
 
     {:noreply, updated_state}
   end
 
+  def handle_cast({:remove_player, player_id}, state) do
+    updated_state =
+      Map.update!(state, :players, fn player_list ->
+        List.delete(player_list, player_id)
+      end)
+
+    {:noreply, updated_state}
+  end
+
+  def handle_cast({:kill_player, player_id}, state) do
+    updated_state =
+      Map.update!(state, :alive_players, fn player_list ->
+        List.delete(player_list, player_id)
+      end)
+
+    {:noreply, updated_state}
+  end
+
+  def handle_cast({:revive_player, player_id}, state) do
+    updated_state =
+      Map.update!(state, :alive_players, fn player_list ->
+        [player_id | player_list]
+        |> Enum.uniq()
+      end)
+
+    {:noreply, updated_state}
+  end
+
+  @impl true
   def handle_call(:get_players, _from, state) do
-    {:reply, state, state}
+    {:reply, state.players, state}
   end
 
-  def handle_info({:remove_player, player_id}, state) do
-    updated_state = List.delete(state, player_id)
-    {:noreply, updated_state}
+  def handle_call(:get_player_count, _from, state) do
+    {:reply, length(state.players), state}
+  end
+
+  @impl true
+  def handle_call(:draw_water, _from, state) do
+    case state.water_deck do
+      [water_card | remaining_deck] ->
+        updated_state = %{state | water_deck: remaining_deck}
+        {:reply, {:ok, water_card}, updated_state}
+
+      [] ->
+        {:reply, :deck_empty, state}
+    end
+  end
+
+  @impl true
+  def handle_call(:scavenge, _from, state) do
+    case state.scavenge_deck do
+      [scavenge_card | remaining_deck] ->
+        updated_state = %{state | scavenge_deck: remaining_deck}
+        {:reply, {:ok, scavenge_card}, updated_state}
+
+      [] ->
+        {:reply, :deck_empty, state}
+    end
   end
 end
